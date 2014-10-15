105f107e7c330ec6688786f0de3f079c235eed87
meanprogrammer <valiantdudz@yahoo.com>
2014-10-15 22:58:23 +0800

    sms data

---
+++ SMS/SMSDataHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace SMS
+{
+    public class SMSDataHelper
+    {
+        Database db;
+        public SMSDataHelper() {
+            db = DatabaseFactory.CreateDatabase("default");
+        }
+
+        public string GetAccessToken(string subscriberNumber) {
+            DbCommand cmd = db.GetSqlStringCommand(
+                            string.Format("SELECT AccessToken FROM AccessTokens WHERE SubscriberNumber = '{0}'",
+                            subscriberNumber));
+            var at = db.ExecuteScalar(cmd).ToString();
+            return at;
+        }
+    }
+}
--- SMS/SMSHelper.cs
+++ SMS/SMSHelper.cs
@@ -8,14 +8,15 @@ namespace SMS
 {
     public class SMSHelper
     {
-        const string APP_ID = "";
-        const string APP_SECRET = "";
-        const string SHORTCODE = "";
+        const string APP_ID = "ekz5qSALXAzhMoT6XBcXMKh75zrqSBbb";
+        const string APP_SECRET = "9e6fc50f715cedbe655c285fbd6efe53033112251ab3b3776424dfc18cdf88a6";
+        const string SHORTCODE = "21580262";
 
         private GlobeLabs api = null;
-
+        private SMSDataHelper data = null;
         public SMSHelper() {
-            api = new GlobeLabs("");
+            api = new GlobeLabs(APP_ID, APP_SECRET);
+            data = new SMSDataHelper();
         }
 
         public string SendSMS(string number, string message) { 
@@ -25,8 +26,22 @@ namespace SMS
                 Message = message,
                 Numbers = nbs
             };
+
+            string at = data.GetAccessToken(number);
+            if (!string.IsNullOrEmpty(at)) {
+                api.AccessToken = at;
+            }
+
             var result = api.PushSms(SHORTCODE, payload);
             return result.Status.StatusCode.ToString();
+        }
+
+        public string AuthorizeNumber(string code) {
+            AuthResult result = api.Authorize(code);
+            if (result != null) { 
+
+            }
+            return string.Empty;
         }
 
 
