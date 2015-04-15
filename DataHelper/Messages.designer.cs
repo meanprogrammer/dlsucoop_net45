1fc827daab6ef864483f51d6bb1993c64f0e6970
meanprogrammer <valiantdudz@yahoo.com>
2015-04-15 22:34:51 +0800

    changw

--- DataHelper/Messages.designer.cs
+++ DataHelper/Messages.designer.cs
@@ -78,7 +78,7 @@ namespace DataHelper
     #endregion
 		
 		public MessagesDataContext() : 
-				base(global::DataHelper.Properties.Settings.Default.PROD, mappingSource)
+				base(global::DataHelper.Properties.Settings.Default.OFFICE, mappingSource)
 		{
 			OnCreated();
 		}
